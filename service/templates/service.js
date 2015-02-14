159dce234ee034f43c468ba845d0040c5d6f8557
Franci Zidar <francizidar@Francis-MacBook-Pro.local>
2015-02-14 17:16:41 +0100

    Changed service name

--- service/templates/service.js
+++ service/templates/service.js
@@ -2,5 +2,7 @@ angular.module('<%= appname %>').factory('<%= _.camelize(name) %>',function() {
 
     var <%= _.camelize(name) %> = {};
 
+    var test = [];
+
     return <%= _.camelize(name) %>;
 });
